f95799e8cf7ecc939b146356d534a8354cccbb18
Angel172ir16 <33010380+Angel172ir16@users.noreply.github.com>
2020-10-02 15:28:24 +0300

    Update durexDB.lua

--- durexDB.lua
+++ durexDB.lua
@@ -6,45 +6,42 @@ function DurexDatabase:new(token)
 
 	local obj= {}
         obj.token = token
-	obj.url = "http://router662.pythonanywhere.com/"
+	obj.url = "http://router662.pythonanywhere.com/users/"
 
 	function obj:setToken(token)
 		self.token = token
 	end
 
 	function obj:get(nick)
-		for temp in internet.request(self.url.."users/get?name="..nick.."&token="..self.token) do      
+		for temp in internet.request(self.url.."get?name="..nick.."&token="..self.token) do      
+			return tonumber(temp)
+		end
+	end
+
+	function obj:getTime()
+		for temp in internet.request("http://durex77.pythonanywhere.com/get/time") do      
 			return tonumber(temp)
 		end
 	end
 
 	function obj:pay(nick,money)
-		for temp in internet.request(self.url.."users/pay?name="..nick.."&token="..self.token.."&money="..money) do
+		for temp in internet.request(self.url.."pay?name="..nick.."&token="..self.token.."&money="..money) do
 			return temp == "True"
 		end
 	end
 
 	function obj:give(nick,money)
-		for temp in internet.request(self.url.."users/give?name="..nick.."&token="..self.token.."&money="..money) do
+		for temp in internet.request(self.url.."give?name="..nick.."&token="..self.token.."&money="..money) do
 			return temp == "True"
 		end
 	end
-
+  
   function obj:top()
-    local result = ""
-		for temp in internet.request(self.url.."krov/users/top") do
-			result = result .. temp
-		end
-    result = serialization.unserialize(result)
-    return result
-	end
-
-  function obj:time()
-    for temp in internet.request(self.url.."krov/get/time") do
-			return tonumber(temp)
+		for temp in internet.request(self.url.."top") do
+			return serialization.unserialize(temp)
 		end
 	end
-
+  
 	setmetatable(obj, self)
 	self.__index = self; return obj
 end
